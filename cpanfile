8b9739113e618d1f4d7b44245ec91b24d6e8191d
Mitch Broadhead <mitch.broadhead@gmail.com>
2018-03-02 01:21:13 -0700

    Finishes MVP

--- cpanfile
+++ cpanfile
@@ -1,6 +1,7 @@
 requires 'perl', '5.008005';
 
 # requires 'Some::Module', 'VERSION';
+requires 'Rex';
 
 on test => sub {
     requires 'Test::More', '0.96';
--- lib/Rex/Vagrant.pm
+++ lib/Rex/Vagrant.pm
@@ -1,8 +1,65 @@
 package Rex::Vagrant;
+use Rex::Commands;
+use Rex::Config;
 
 use strict;
 use 5.008_005;
 our $VERSION = '0.01';
+
+our $VAGRANT_SSH_CONFIG_FOR;
+
+sub import {
+  my ( $class, $what, $addition ) = @_;
+  $what =~ s/^-//;
+
+  if ( $what =~ /^env(ironment)?$/ ) {
+    $addition //= "vagrant";
+    $class->setup_environment($addition);
+  }
+  elsif ( $what eq "groups" ) {
+    $class->setup_ssh_config;
+    $class->setup_groups;
+  }
+}
+
+sub ssh_config {
+  my ( $class, $refetch ) = @_;
+  if ( $refetch || !defined $VAGRANT_SSH_CONFIG_FOR ) {
+    my @cfg = `vagrant ssh-config`;
+    die "failed to obtain vagrant ssh-config!" unless $? == 0;
+    $VAGRANT_SSH_CONFIG_FOR = { Rex::Config->_parse_ssh_config(@cfg) };
+  }
+  return $VAGRANT_SSH_CONFIG_FOR;
+}
+
+sub setup_ssh_config {
+  my $class      = shift;
+  my $ssh_config = $class->ssh_config;
+  @Rex::Config::SSH_CONFIG_FOR{ keys %{$ssh_config} } = values %{$ssh_config};
+
+  # @TODO: Set these based on values from $cfg and only if rex is using openssh
+  Rex::Config->set_openssh_opt(
+    StrictHostKeyChecking => "no",
+    UserKnownHostsFile    => "/dev/null",
+    LogLevel              => "QUIET"
+  );
+}
+
+sub setup_groups {
+  my $class      = shift;
+  my $ssh_config = $class->ssh_config;
+  group $_ => $_ for keys %{$ssh_config};
+  group "all" => keys %{$ssh_config};
+}
+
+sub setup_environment {
+  my ( $class, $env_name ) = @_;
+  environment $env_name => sub {
+
+    $class->setup_ssh_config;
+    $class->setup_groups;
+  };
+}
 
 1;
 __END__
