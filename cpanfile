b661c91d09fe535932965746165ef8a0ced097f3
Mitch Broadhead <mbroadhead@nationalprocessing.com>
2020-10-05 09:47:17 -0600

    Fixes this module to work with (R)?ex 1.12.2. Adds caching support.

--- cpanfile
+++ cpanfile
@@ -1,7 +1,7 @@
 requires 'perl', '5.008005';
 
-# requires 'Some::Module', 'VERSION';
 requires 'Rex';
+requires 'Path::Tiny';
 
 on test => sub {
     requires 'Test::More', '0.96';
--- lib/Rex/Vagrant.pm
+++ lib/Rex/Vagrant.pm
@@ -1,65 +1,126 @@
 package Rex::Vagrant;
 use Rex::Commands;
 use Rex::Config;
+use Path::Tiny ();
+use Data::Dumper;
 
 use strict;
 use 5.008_005;
-our $VERSION = '0.01';
+# our $VERSION = '0.01';
 
-our $VAGRANT_SSH_CONFIG_FOR;
+our (
+  $VAGRANT_SSH_CONFIG_FOR,
+  $cache_ssh_config,
+  $cache_timeout,
+);
+
+# Anytime you import Rex::Config it re-reads the default ssh config file and
+# stomps on our custom settings. To prevent this, we add a monkey-patch that
+# re-adds our custom vagrant ssh-config anytime this module gets imported.
+my $rex_config_import = \&Rex::Config::import;
+*Rex::Config::import = sub {
+  $rex_config_import->(@_);
+  if ( defined $VAGRANT_SSH_CONFIG_FOR ) {
+    @Rex::Config::SSH_CONFIG_FOR{ keys %$VAGRANT_SSH_CONFIG_FOR } =
+      values %$VAGRANT_SSH_CONFIG_FOR;
+  }
+};
 
 sub import {
   my ( $class, @opts ) = @_;
+
+  my ($env_opts, $group_opts);
 
   while ( my $feature = shift @opts ) {
     $feature =~ s/^-//;
     my $feature_opts = ref $opts[0] eq "HASH" ? shift @opts : {};
     if ( $feature =~ qr/^env(ironment)?$/ ) {
-      $class->setup_environment(%$feature_opts);
+      $env_opts = $feature_opts;
     }
     elsif ( $feature =~ qr/^groups$/ ) {
-      $class->setup_ssh_config;
-      $class->setup_groups(%$feature_opts);
+      $group_opts = $feature_opts;
+    }
+    elsif ($feature =~ qr/^cache$/) {
+      $cache_ssh_config = $feature_opts->{enabled};
+      $cache_timeout = $feature_opts->{timeout};
     }
   }
+
+  $class->setup_environment(%$env_opts) if defined $env_opts;
+  $class->setup_groups(%$group_opts) if defined $group_opts;
+
 }
 
-sub ssh_config {
-  my ( $class, %opt ) = @_;
-  if ( $opt{refetch} || !defined $VAGRANT_SSH_CONFIG_FOR ) {
-    my @cfg = `vagrant ssh-config`;
-    die "failed to obtain vagrant ssh-config!" unless $? == 0;
-    $VAGRANT_SSH_CONFIG_FOR = { Rex::Config->_parse_ssh_config(@cfg) };
+sub vagrant_ssh_config {
+  my $class = shift;
+
+  return $VAGRANT_SSH_CONFIG_FOR if defined $VAGRANT_SSH_CONFIG_FOR;
+
+  die
+    "\$::rexfile not defined. This module should only be used inside a Rexfile!"
+    unless defined $::rexfile;
+
+  my $app_dir = Path::Tiny::path($::rexfile)->parent->absolute;
+
+  if ($cache_ssh_config) {
+    my $vagrant_dir = $app_dir->child('.vagrant');
+    die "Cannot cache `vagrant ssh-config`: no such directory: $vagrant_dir."
+      unless $vagrant_dir->is_dir;
+    $vagrant_dir->child('.rex')->mkpath;
+    my $cache_file = $vagrant_dir->child('.rex/ssh-config');
+    if (
+      !$cache_file->is_file
+      || ( defined $cache_timeout
+        && (time - $cache_file->stat->mtime) >= $cache_timeout )
+      )
+    {
+      my @cfg = `cd $app_dir && vagrant ssh-config`;
+      $cache_file->spew(@cfg);
+      return $VAGRANT_SSH_CONFIG_FOR = { Rex::Config->_parse_ssh_config(@cfg) };
+    }
+    else {
+      return $VAGRANT_SSH_CONFIG_FOR = { Rex::Config->_parse_ssh_config($cache_file->lines) };
+    }
   }
-  return $VAGRANT_SSH_CONFIG_FOR;
+
+  my @cfg = `cd $app_dir && vagrant ssh-config`;
+  return $VAGRANT_SSH_CONFIG_FOR = { Rex::Config->_parse_ssh_config(@cfg) };
 }
 
 sub setup_ssh_config {
-  my $class      = shift;
-  my $ssh_config = $class->ssh_config;
-  @Rex::Config::SSH_CONFIG_FOR{ keys %{$ssh_config} } = values %{$ssh_config};
+  my ($class, %opt) = @_;
+  my $ssh_config = $class->vagrant_ssh_config(%opt);
+  for my $host (keys %$ssh_config) {
+    $Rex::Config::SSH_CONFIG_FOR{$host} = $ssh_config->{$host};
+  }
 
   # @TODO: Set these based on values from $cfg and only if rex is using openssh
-  Rex::Config->set_openssh_opt(
-    StrictHostKeyChecking => "no",
-    UserKnownHostsFile    => "/dev/null",
-    LogLevel              => "QUIET"
-  );
+  # Rex::Config->set_openssh_opt(
+  #   StrictHostKeyChecking => "no",
+  #   UserKnownHostsFile    => "/dev/null",
+  #   LogLevel              => "QUIET"
+  # );
 }
 
 sub setup_groups {
   my ( $class, %opt ) = @_;
-  my $ssh_config = $class->ssh_config;
+  my $ssh_config = $class->vagrant_ssh_config(%opt);
+
+  $class->setup_ssh_config();
+
+  # We have to create groups that point to the host from the ssh config, not
+  # the hostname. That way rex can find the ssh config for it.
+  my %groups = map { $_ => $_ } keys %$ssh_config;
 
   # Create the group with all hosts
   # Dont create the group if { all => undef } is supplied.
   if ( !exists $opt{all} || defined $opt{all} ) {
     my $name = $opt{all} || "all";
-    group $name => keys %{$ssh_config};
+    group $name => values %groups;
   }
 
   # create group for each host
-  group $_ => $_ for keys %{$ssh_config};
+  group $_ => $groups{$_} for keys %groups;
 }
 
 sub setup_environment {
@@ -92,6 +153,13 @@ Rex::Vagrant - Easily interact with your Vagrant boxes through Rex.
   # Same as above but customize the environment name and the "all" group name
   use Rex::Vagrant -env =>
     { name => "my-env", groups => { all => "all-my-vagrant-boxes" } };
+
+  # Setup the default environment and cache the `vagrant ssh-config` for
+  # 5 minutes. This helps speed up running `rex -T`, or any other rex
+  # invocation when the ssh-config is cached.
+  use Rex::Vagrant
+    -env => {},
+    -cache => { enabled => 1, timeout => 300 };
 
   # Create groups and correct auth for all vagrant hosts directly without using
   # an environment. This makes interacting with rex slow because it has to
